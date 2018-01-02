4b9cae2bae1f375af97b53074cc4a5316c5ca8f5
104021097 <104021097@live.asia.edu>
2018-01-02 12:57:09 +0800

    1

---
+++ src/DrawFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class DrawFrame extends JFrame {
+    private JButton jbtn_exit = new JButton("Exit");
+
+
+
+
+    public DrawFrame(){
+        initComp();
+    }
+    private void initComp(){
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SFrame sfrm = new SFrame();
+        sfrm.setVisible(true);
+        CFrame cfrm = new CFrame();
+        cfrm.setVisible(true);
+    }
+}
